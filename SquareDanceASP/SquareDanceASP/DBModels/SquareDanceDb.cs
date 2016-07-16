119ecf1a1b8b067a12f8aa65064b8658e31fa3f2
EnhuiGuo <eguo@harding.edu>
2016-07-15 19:13:51 -0500

    Video Project
    Create On 7-12-2016

---
+++ SquareDanceASP/SquareDanceASP/DBModels/SquareDanceDb.cs
@@ -0,0 +1,20 @@
+﻿using SquareDanceASP.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace SquareDanceASP.DBModels
+{
+    public class SquareDanceDb : ApplicationDbContext
+    {
+        public SquareDanceDb()
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
