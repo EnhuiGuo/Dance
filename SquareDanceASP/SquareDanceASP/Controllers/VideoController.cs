925c2f339dc509dd68b7de392358c0d8fd1f480d
EnhuiGuo <eguo@harding.edu>
2016-07-21 21:27:49 -0500

    NavBar Fixed

--- SquareDanceASP/SquareDanceASP/Controllers/VideoController.cs
+++ SquareDanceASP/SquareDanceASP/Controllers/VideoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,7 @@ namespace SquareDanceASP.Controllers
 
         public ActionResult Upload()
         {
+            var userId = User.Identity.GetUserId();
             return View();
         }
     }
